6022ea4475fe7fadc3826574046e0adae06b1ad1
CodeX93 <85872799+CodeX93@users.noreply.github.com>
2025-11-05 17:18:07 +0500

    Food Scans without usageCheck- with scanHistoryFixed

--- src/pages/ScanHistories.tsx
+++ src/pages/ScanHistories.tsx
@@ -13,6 +13,7 @@ import { cn } from "@/lib/utils";
 import { Calendar as CalendarIcon, ChevronDown } from "lucide-react";
 import { supabase } from "@/integrations/supabase/client";
 import { Camera, Search } from "lucide-react";
+import { getImageUrl } from "@/utils/foodScan";
 
 type FoodScan = {
   id: string;
